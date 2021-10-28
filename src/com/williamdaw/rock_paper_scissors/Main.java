a54595afb586222efdc8d1302ea805993c21be98
DoryBoy <williamodaw@outlook.com>
2021-10-29 00:43:57 +0100

    updates

--- src/com/williamdaw/rock_paper_scissors/Main.java
+++ src/com/williamdaw/rock_paper_scissors/Main.java
@@ -7,42 +7,35 @@ import static java.lang.Boolean.FALSE;
 import static java.lang.Boolean.TRUE;
 
 public class Main {
-
     public static void main(String[] args) {
-//        String[] valid_guess = {"S", "P", "R"};
-        ArrayList<String> valid_guess = new ArrayList<>();
-        valid_guess.add("R");
-        valid_guess.add("P");
-        valid_guess.add("S");
+        String valid_guess = "SPR";
         int pc_wins = 0;
         int user_wins = 0;
-        
+        int turns = 0;
 
-        String input = "";
-        boolean valid_input = FALSE;
-        while (valid_input == FALSE) {
-            System.out.println("Enter R, S or P:");
-            Scanner scanner = new Scanner(System.in);
-            input = scanner.nextLine().toUpperCase();
-            if ((input.length() > 0)&&(valid_guess.contains(input))&&(input.length() < 2)) valid_input = TRUE;
+        while (turns < 3){
+            String input = "";
+            boolean valid_input = FALSE;
+            while (valid_input == FALSE) {
+                System.out.println("Enter R, S or P:");
+                Scanner scanner = new Scanner(System.in);
+                input = scanner.nextLine().toUpperCase();
+                if ((input.length() > 0)&&(valid_guess.contains(input))&&(input.length() < 2)) valid_input = TRUE;
+            }
+            int random_number = (int) (Math.random() * (3 + 1) - 1);
+            String ai_choice = String.valueOf(valid_guess.toCharArray()[random_number]);
+
+            System.out.println(input);
+            System.out.println(ai_choice);
+
+            if (input.equals(ai_choice)) System.out.println("draw");
+            if ((input.equals("R"))&&(ai_choice.equals("S"))) { System.out.println("You win"); turns++; user_wins++; }
+            if ((input.equals("S"))&&(ai_choice.equals("R"))) { System.out.println("You lose"); turns++; pc_wins++; }
+            if ((input.equals("P"))&&(ai_choice.equals("R"))) { System.out.println("You win"); turns++; user_wins++; }
+            if ((input.equals("R"))&&(ai_choice.equals("P"))) { System.out.println("You lose"); turns++; pc_wins++; }
+            if ((input.equals("S"))&&(ai_choice.equals("P"))) { System.out.println("You win"); turns++; user_wins++; }
+            if ((input.equals("P"))&&(ai_choice.equals("S"))) { System.out.println("You lose"); turns++; pc_wins++; }
         }
-        int random_number = (int) (Math.random() * (3 + 1) - 1);
-        String ai_choice = valid_guess.get(random_number);
-        System.out.println(ai_choice);
-
-        if (input.equals(ai_choice)) System.out.println("draw");
-        if ((input.equals("R"))&&(ai_choice.equals("S"))) user_wins++; System.out.println("You win") ;
-        if ((input.equals("S"))&&(ai_choice.equals("R"))) pc_wins++; System.out.println("You lose");
-        if ((input.equals("P"))&&(ai_choice.equals("R"))) user_wins++; System.out.println("You win");
-        if ((input.equals("R"))&&(ai_choice.equals("P"))) pc_wins++; System.out.println("You lose");
-        if ((input.equals("S"))&&(ai_choice.equals("P"))) user_wins++; System.out.println("You win");
-        if ((input.equals("P"))&&(ai_choice.equals("S"))) pc_wins++; System.out.println("You lose");
-
-
-
         System.out.print("You scored " + user_wins + " the computer scored " + pc_wins);
-
-
-
     }
 }
